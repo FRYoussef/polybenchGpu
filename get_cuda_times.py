24c5f254a80c74f980ea2eeca623656fcf78b9da
FRYoussef <f.r.youssef@hotmail.com>
2023-05-17 17:15:56 +0200

    fixed small bug

--- get_cuda_times.py
+++ get_cuda_times.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
         os.rename(os.path.join(data_path, out_file), os.path.join(data_path, f'{out_file}_{time.time()}'))
 
     test_folder: str = os.path.join('polybenchCodesCudaOpenClHMPPOpenAcc', 'CUDA')
-    tests: List[str] = ["2DCONV", "2MM", "3DCONV", "3MM", "ATAX", "BICG", "CORR", "COVAR", "FDTD-2D", "GEMM", "GESUMMV", "MVT", "SYR2K", "SYRK"]
-    names: List[str] = ["2DConvolution", "2mm", "3DConvolution", "3mm", "Atax", "Bicg", "Correlation", "Covariance", "Fdtd2d", "Gemm", "Gesummv", "Mvt", "Syr2k", "Syrk"]    
+    tests: List[str] = ["2DCONV", "2MM", "3DCONV", "3MM", "ATAX", "BICG", "CORR", "COVAR", "FDTD-2D", "GEMM", "GESUMMV", "GRAMSCHM", "MVT", "SYR2K", "SYRK"]
+    names: List[str] = ["2DConvolution", "2mm", "3DConvolution", "3mm", "Atax", "Bicg", "Correlation", "Covariance", "Fdtd2d", "Gemm", "Gesummv", "Gramschm", "Mvt", "Syr2k", "Syrk"]    
     params: List[int] = [4096, 512, 512, 512, 4096, 8192, 512, 512, 512, 512, 4096, 512, 8192, 512, 512]
     times: Dict[str, List[float]] = dict()
     std_devs: List[float] = list()
